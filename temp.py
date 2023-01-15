b40de95c6c699cc07bdff2e3473afd52c3bcaace
u7016520 <sai.jasti.99@gmail.com>
2023-01-16 01:09:42 +1000

    Adding notes.txt

--- temp.py
+++ temp.py
@@ -1,2 +1,15 @@
-print("xzy")
-print("FLAG1")
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+
+class Discriminator(nn.Module):
+    def __init__(self, in_feature):
+        super().__init__()
+        self.disc = nn.Sequential(
+            nn.Linear(in_feature, 128)
+        )
