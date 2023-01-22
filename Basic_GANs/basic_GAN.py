d1e7e5fc79fac1e65c48b28ffa58faf09979a4b2
u7016520 <sai.jasti.99@gmail.com>
2023-01-22 16:13:01 +1000

    basic_discriminator_and_generator

--- Basic_GANs/basic_GAN.py
+++ Basic_GANs/basic_GAN.py
@@ -46,3 +46,22 @@ z_dim = 64
 image_dim = 28 * 28 * 1  # 784
 batch_size = 32
 num_epochs = 50
+
+disc = Discriminator(image_dim).to(device)
+gen = Generator(z_dim, image_dim).to(device)
+fixed_noise = torch.randn((batch_size, z_dim)).to(device)
+transforms = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)),
+    ]
+)
+
+dataset = datasets.MNIST(root="dataset/", transform=transforms, download=True)
+loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+opt_disc = optim.Adam(disc.parameters(), lr=lr)
+opt_gen = optim.Adam(gen.parameters(), lr=lr)
+criterion = nn.BCELoss()
+writer_fake = SummaryWriter(f"logs/fake")
+writer_real = SummaryWriter(f"logs/real")
+step = 0
